13bac74faa9e33ec65291f2c435f348ddb8f75fe
yie1d <myie1d@163.com>
2025-05-28 16:44:16 +0800

    update

--- scripts/label_studio_to_llama_mllm.py
+++ scripts/label_studio_to_llama_mllm.py
@@ -1,11 +1,59 @@
 import json
-import time
+import math
+
 from copy import deepcopy
 from pathlib import Path
-from PIL import ImageDraw, Image
 
 SYSTEM_PROMPT_1 = "基于给你的截图，我给你一个操作需求，你给我返回相关操作元素的bbox坐标，以JSON格式输出其bbox坐标"
 SYSTEM_PROMPT_2 = "基于给你的截图，我给你一个操作需求，你给我返回相关操作元素的bbox坐标，以XML格式输出其坐标 <points x y>object</points>"
+
+
+# This is the resize function of Qwen2.5-VL
+def smart_resize(
+    height: int, width: int, factor: int = 28, min_pixels: int = 56 * 56, max_pixels: int = 14 * 14 * 4 * 1280
+):
+    """Rescales the image so that the following conditions are met:
+    1. Both dimensions (height and width) are divisible by 'factor'.
+    2. The total number of pixels is within the range ['min_pixels', 'max_pixels'].
+    3. The aspect ratio of the image is maintained as closely as possible.
+    """
+    if height < factor or width < factor:
+        raise ValueError(f"height:{height} or width:{width} must be larger than factor:{factor}")
+    elif max(height, width) / min(height, width) > 200:
+        raise ValueError(
+            f"absolute aspect ratio must be smaller than 200, got {max(height, width) / min(height, width)}"
+        )
+    h_bar = round(height / factor) * factor
+    w_bar = round(width / factor) * factor
+    if h_bar * w_bar > max_pixels:
+        beta = math.sqrt((height * width) / max_pixels)
+        h_bar = math.floor(height / beta / factor) * factor
+        w_bar = math.floor(width / beta / factor) * factor
+    elif h_bar * w_bar < min_pixels:
+        beta = math.sqrt(min_pixels / (height * width))
+        h_bar = math.ceil(height * beta / factor) * factor
+        w_bar = math.ceil(width * beta / factor) * factor
+    return h_bar, w_bar
+
+
+def convert_to_qwen25vl_format(bbox, orig_height, orig_width, factor=28, min_pixels=56 * 56,
+                               max_pixels=14 * 14 * 4 * 1280):
+    new_height, new_width = smart_resize(orig_height, orig_width, factor, min_pixels, max_pixels)
+    scale_w = new_width / orig_width
+    scale_h = new_height / orig_height
+
+    x1, y1, x2, y2 = bbox
+    x1_new = round(x1 * scale_w)
+    y1_new = round(y1 * scale_h)
+    x2_new = round(x2 * scale_w)
+    y2_new = round(y2 * scale_h)
+
+    x1_new = max(0, min(x1_new, new_width - 1))
+    y1_new = max(0, min(y1_new, new_height - 1))
+    x2_new = max(0, min(x2_new, new_width - 1))
+    y2_new = max(0, min(y2_new, new_height - 1))
+
+    return [x1_new, y1_new, x2_new, y2_new]
 
 
 def read_json(file_path: Path):
@@ -30,12 +78,21 @@ def main():
         for result_item in item['annotations'][0]['result']:
             if result_item['type'] != 'textarea':
                 continue
-
             result_value = result_item['value']
-            _data = {
+
+            bbox = [
+                result_value["x"] * result_item['original_width'] / 100,
+                result_value["y"] * result_item['original_height'] / 100,
+                (result_value['x'] + result_value["width"]) * result_item['original_width'] / 100,
+                (result_value["y"] + result_value["height"]) * result_item['original_height'] / 100
+            ]
+            bbox = deepcopy(
+                convert_to_qwen25vl_format(bbox, result_item['original_height'], result_item['original_width']))
+
+            output_data.append({
                 "messages": [
                     {
-                        "value": '',
+                        "value": SYSTEM_PROMPT_1,
                         "from": "system"
                     },
                     {
@@ -43,27 +100,16 @@ def main():
                         "from": "human"
                     },
                     {
-                        "value": '',
+                        "value": bbox,
                         "from": "gpt"
                     }
                 ],
                 "images": [
                     f"/root/autodl-tmp/dataset/rpa_action/images/{item['file_upload'].split('-')[-1]}"
                 ]
-            }
+            })
 
-            _data['messages'][0]['value'] = SYSTEM_PROMPT_1
-            _data['messages'][2]['value'] = (f'{{"bbox_2d": ['
-                                             f'{int(result_value["x"] * result_item["original_height"] / 100)}, '
-                                             f'{int(result_value["y"] * result_item["original_width"] / 100)}, '
-                                             f'{int((result_value["x"] + result_value["width"]) * result_item["original_width"] / 100)}, '
-                                             f'{int((result_value["y"] + result_value["height"]) * result_item["original_height"] / 100)}]}}')
-            output_data.append(deepcopy(_data))
-            _data['messages'][0]['value'] = SYSTEM_PROMPT_2
-            _data['messages'][2]['value'] = (f'<points x1="{int((result_value["x"] + result_value["width"] / 2) * result_item["original_width"] / 100)}" '
-                                             f'y1="{int((result_value["y"] + result_value["height"] / 2) * result_item["original_height"] / 100)}">'
-                                             f'alt="{result_value["text"][0]}">{result_value["text"][0]}</points>')
-            output_data.append(deepcopy(_data))
+
 
     write_json(output_data, output_file)
 
